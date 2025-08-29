8955941bc8ef2dc135c563c4c0755860d4007e86
Toukir <toukerahmed.2728@gmail.com>
2025-08-29 16:40:41 +0600

    call functionality add

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
-function takeInt(id){
-const recValue=document.getElementById(id);
-const recValueString=recValue.innerText;
-const recValueInt=parseInt(recValueString);
-return recValueInt;
+function takeInt(id) {
+  const recValue = document.getElementById(id);
+  const recValueString = recValue.innerText;
+  const recValueInt = parseInt(recValueString);
+  return recValueInt;
 }
 
-// heart
 
+// heart
 
 // document.getElementById('heart').addEventListener('click',function(){
 //     // console.log('heart pressed');
@@ -18,15 +18,38 @@ return recValueInt;
 //     document.getElementById('heart_up').innerText=upHeartValueInt;
 // })
 
-const hearts=document.querySelectorAll('.h1');
-for(const hrt of hearts){
-    hrt.addEventListener('click',function(){
-      const id = hrt.getAttribute('target');
-      const heartValue=document.getElementById('heart_up').innerText;
-    const val=takeInt('heart_up');   
-    const upHeartValueInt=val+1;
-    document.getElementById('heart_up').innerText=upHeartValueInt;
-
-    })
+const hearts = document.querySelectorAll(".h1");
+for (const hrt of hearts) {
+  hrt.addEventListener("click", function () {
+    const id = hrt.getAttribute("target");
+    const heartValue = document.getElementById("heart_up").innerText;
+    const val = takeInt("heart_up");
+    const upHeartValueInt = val + 1;
+    document.getElementById("heart_up").innerText = upHeartValueInt;
+  });
 }
+
+// const avialbaleCoin = takeInt("coin");
+// console.log(avialbaleCoin);
+// if (avialbaleCoin < 20) {
+//   alert("Insufficient coins");
+//   return;
+// }
+document.getElementById("ne-call").addEventListener("click", function () {
+    console.log('ne call pressed')
+  const coinValue = takeInt("coin");
+  if (coinValue <= 20) {
+    alert("Insufficient coins");
+    return;
+  } else {
+    alert("avi service says:\ni am ok");
+    const upCoinValue = coinValue - 20;
+    document.getElementById("coin").innerText = upCoinValue;
+  }
+});
+
+
+//   document.getElementById("ne-call").addEventListener("click", function() {
+//     alert("avi service says:\ni am ok");
+//   });
 
